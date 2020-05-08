a707414b2373f89668ffd26ff9f81e6660ce0257
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-09 01:13:20 +0200

    created SearchBooks component inside for now

--- src/App.js
+++ src/App.js
@@ -549,3 +549,28 @@ class ListOfBooks extends React.Component {
 }
 
 //export default ListOfBooks;
+
+class SearchBooks extends React.Component {
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <button
+            className="close-search"
+            onClick={() => this.setState({ showSearchPage: false })}
+          >
+            Close
+          </button>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid" />
+        </div>
+      </div>
+    );
+  }
+}
+
+//export default SearchBooks;
