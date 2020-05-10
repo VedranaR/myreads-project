a49c7a5c61cb3c61501b922fc6e309208c415167
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-11 00:05:03 +0200

    added ShelfChanger component

--- src/App.js
+++ src/App.js
@@ -113,17 +113,6 @@ class Book extends React.Component {
                   'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
               }}
             />
-            <div className="book-shelf-changer">
-              <select>
-                <option value="move" disabled>
-                  Move to...
-                </option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
           </div>
           <div className="book-title">The Hobbit</div>
           <div className="book-authors">J.R.R. Tolkien</div>
@@ -134,3 +123,23 @@ class Book extends React.Component {
 }
 
 //export default Book;
+
+class ShelfChanger extends React.Component {
+  render() {
+    return (
+      <div className="book-shelf-changer">
+        <select>
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+//export default ShelfChanger;
