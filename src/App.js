42c268c868fa46ea107bd6ed0e8687f768676999
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-12 16:35:15 +0200

    mover SearchBooks to the end to split it

--- src/App.js
+++ src/App.js
@@ -41,31 +41,6 @@ class ListOfBooks extends React.Component {
 
 //export default ListOfBooks;
 
-class SearchBooks extends React.Component {
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <button
-            className="close-search"
-            onClick={() => this.setState({ showSearchPage: false })}
-          >
-            Close
-          </button>
-          <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid" />
-        </div>
-      </div>
-    );
-  }
-}
-
-//export default SearchBooks;
-
 class Bookshelves extends React.Component {
   render() {
     const { bookshelves } = this.props;
@@ -146,3 +121,28 @@ class ShelfChanger extends React.Component {
 }
 
 //export default ShelfChanger;
+
+class SearchBooks extends React.Component {
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <button
+            className="close-search"
+            onClick={() => this.setState({ showSearchPage: false })}
+          >
+            Close
+          </button>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid" />
+        </div>
+      </div>
+    );
+  }
+}
+
+//export default SearchBooks;
