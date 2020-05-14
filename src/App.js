a5ff8f1332e38bd80b75d005d200e0750751a658
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-15 00:02:24 +0200

    thumbnail for each individual Book

--- src/App.js
+++ src/App.js
@@ -108,8 +108,7 @@ class Book extends React.Component {
               style={{
                 width: 128,
                 height: 192,
-                backgroundImage:
-                  'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
+                backgroundImage: `url(${book.imageLinks.smallThumbnail})`,
               }}
             />
             <ShelfChanger />
