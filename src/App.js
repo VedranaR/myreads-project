f84de0868918bd23853a5489b5226dd72991a299
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-14 23:56:51 +0200

    map through books and render each Book

--- src/App.js
+++ src/App.js
@@ -84,7 +84,9 @@ class Bookshelf extends React.Component {
         <h2 className="bookshelf-title">{shelf.name}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <Book />
+            {booksOnShelf.map((book) => (
+              <Book book={book} shelf={shelf.key} />
+            ))}
           </ol>
         </div>
       </div>
