acb4910315fe08bdbd10ff6fca80a7bccf6a99b9
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-16 23:58:36 +0200

    separated Book into the separate component

--- src/App.js
+++ src/App.js
@@ -38,33 +38,6 @@ class BooksApp extends React.Component {
 
 export default BooksApp;
 
-class Book extends React.Component {
-  render() {
-    const { book, shelf } = this.props;
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 192,
-                backgroundImage: `url(${book.imageLinks.smallThumbnail})`,
-              }}
-            />
-            <ShelfChanger book={book} shelf={shelf} />
-          </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors.join(", ")}</div>
-        </div>
-      </li>
-    );
-  }
-}
-
-//export default Book;
-
 class ShelfChanger extends React.Component {
   render() {
     const { book, shelf } = this.props;
