bf5fa68083ab7c40f13ff5a684c2e69fbba9ee72
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-17 00:18:34 +0200

    destructured books from props

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -3,6 +3,7 @@ import Book from "./Book";
 
 class SearchResults extends React.Component {
   render() {
+    const { books } = this.props;
     return (
       <div className="search-books-results">
         <ol className="books-grid" />
