a31b7426881bd3bc458bc5e2b7a1af6d66ddfff8
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-17 00:00:37 +0200

    separated ShelfChanger into the separate component

--- src/App.js
+++ src/App.js
@@ -38,27 +38,6 @@ class BooksApp extends React.Component {
 
 export default BooksApp;
 
-class ShelfChanger extends React.Component {
-  render() {
-    const { book, shelf } = this.props;
-    return (
-      <div className="book-shelf-changer">
-        <select value={shelf}>
-          <option value="move" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    );
-  }
-}
-
-//export default ShelfChanger;
-
 class SearchBooks extends React.Component {
   render() {
     return (
---
+++ src/ShelfChanger.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class ShelfChanger extends React.Component {
+  render() {
+    const { book, shelf } = this.props;
+    return (
+      <div className="book-shelf-changer">
+        <select value={shelf}>
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+//export default ShelfChanger;
