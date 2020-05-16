465394326d409f58902d58c199d488d28a17e457
Vedrana <vedrana_ravlic16@yahoo.com>
2020-05-16 23:57:00 +0200

    separated Bookshelf into the separate component

--- src/App.js
+++ src/App.js
@@ -38,27 +38,6 @@ class BooksApp extends React.Component {
 
 export default BooksApp;
 
-class Bookshelf extends React.Component {
-  render() {
-    const { shelf, books } = this.props;
-    const booksOnShelf = books.filter((book) => book.shelf === shelf.key);
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelf.name}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {booksOnShelf.map((book) => (
-              <Book key={book.id} book={book} shelf={shelf.key} />
-            ))}
-          </ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-//export default Bookshelf;
-
 class Book extends React.Component {
   render() {
     const { book, shelf } = this.props;
---
+++ src/Bookshelf.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Bookshelf extends React.Component {
+  render() {
+    const { shelf, books } = this.props;
+    const booksOnShelf = books.filter((book) => book.shelf === shelf.key);
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelf.name}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {booksOnShelf.map((book) => (
+              <Book key={book.id} book={book} shelf={shelf.key} />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+//export default Bookshelf;
